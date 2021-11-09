56088440c9745d43c5f89f02ef5ab24ead9e81f5
Kim Junil <iam@junil.kim>
2021-11-10 01:15:17 +0900

    fix call dispatches

--- train.py
+++ train.py
@@ -72,14 +72,13 @@ def send_message_to_slack(url, acc, loss, training_time, model_path):
 def request_deploy_api(model_path):
     owner = os.getenv("GITHUB_OWNER")
     repo = os.getenv("GITHUB_REPO")
+    workflow_id = os.getenv("GITHUB_WORKFLOW")
     access_token = os.getenv("GITHUB_TOKEN")
     model_tag = os.getenv("MODEL_TAG")
 
-    print(owner, repo, model_tag)
-
     headers = {'Authorization' : 'token ' + access_token }
     data = {"model_path": model_path, "model_tag": model_tag }
-    r = requests.post(f"http://api.github.com/repos/{owner}/{repo}/dispatches", headers=headers, data=data)
+    r = requests.post(f"http://api.github.com/repos/{owner}/{repo}/actions/workflows/{workflow_id}/dispatches", headers=headers, data=data)
     print(r)
 
 
